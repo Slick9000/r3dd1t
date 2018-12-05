8f15708eb4c515a3bbb6849063000221275f9de0
Slick9000 <37941291+Slick9000@users.noreply.github.com>
2018-12-04 21:47:58 -0400

    Add sub unfound error

--- reddit.py
+++ reddit.py
@@ -31,58 +31,65 @@ async def on_message(msg):
 @bot.command()
 async def sub(ctx, sub = None):
     """The reddit command."""
-
-    color = 0xff4500
+    try:
+        color = 0xff4500
     
-    if sub == None:
+        if sub == None:
         
-        owner = discord.utils.get(bot.users, id=357641367507435531)
+            owner = discord.utils.get(bot.users, id=357641367507435531)
         
-        embed = discord.Embed(color=color)
+            embed = discord.Embed(color=color)
         
-        embed.add_field(name="R3dd1t", value="Thanks for inviting Reddit Bot to your server!\n"
-                                             "This bot essentially gets images from any subreddit you specify.\n"
-                                             "Try `~sub hamsters` or `~sub JoyconBoyz`."
-                        )
+            embed.add_field(name="R3dd1t", value="Thanks for inviting Reddit Bot to your server!\n"
+                                                 "This bot essentially gets images from any subreddit you specify.\n"
+                                                 "Try `~sub hamsters` or `~sub JoyconBoyz`."
+                           )
         
-        embed.add_field(name="Author", value=f"This bot was created by {owner.mention} in discord.py.")
+            embed.add_field(name="Author", value=f"This bot was created by {owner.mention} in discord.py.")
         
-        embed.add_field(name="Bot invite:", value=f"https://discordapp.com/oauth2/authorize?client_id={bot.user.id}"
-                                                   "&scope=bot&permissions=18432"
-                        )
+            embed.add_field(name="Bot invite:", value=f"https://discordapp.com/oauth2/authorize?client_id={bot.user.id}"
+                                                       "&scope=bot&permissions=18432"
+                           )
         
-        embed.add_field(name="Embed Errors", value="If the link isn't an image, it will drop the embed and just post the link.\n"
-                              "(Blame reddit API for this :c)"
-                        )
+            embed.add_field(name="Embed Errors", value="If the link isn't an image, it will drop the embed and just post the link.\n"
+                                 "(Blame reddit API for this :c)"
+                           )
         
-        embed.set_image(url="https://cdn.discordapp.com/attachments/507339242096033792/519655500216926208/885444_news_512x512.png")
+            embed.set_image(url="https://cdn.discordapp.com/attachments/507339242096033792/519655500216926208/885444_news_512x512.png")
         
-        await ctx.send(embed=embed)
-        return
+            await ctx.send(embed=embed)
+            return
 
-    async with aiohttp.ClientSession() as cs:
-        async with cs.get(f"https://api.reddit.com/r/{sub}/random") as r:
-            data = await r.json()
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(f"https://api.reddit.com/r/{sub}/random") as r:
+                data = await r.json()
 
-            # is_self checks if it's a text post, which is what we don't want
-            if data[0]["data"]["children"][0]["data"]["is_self"] == False:
+                # is_self checks if it's a text post, which is what we don't want
+                if data[0]["data"]["children"][0]["data"]["is_self"] == False:
                 
-                image = data[0]["data"]["children"][0]["data"]["url"]
+                    image = data[0]["data"]["children"][0]["data"]["url"]
                 
-                embed = discord.Embed(
-                    title=data[0]["data"]["children"][0]["data"]["subreddit"],
-                    color=color
-                    )
+                    embed = discord.Embed(
+                        title=data[0]["data"]["children"][0]["data"]["subreddit"],
+                        color=color
+                        )
                 
-                embed.set_image(url=image)
+                    embed.set_image(url=image)
                 
-                await ctx.send(embed=embed)
+                    await ctx.send(embed=embed)
                 
-            else:
-                # if reddit api still gives a text post i'll just post it
-                link = data[0]["data"]["children"][0]["data"]["url"]
+                else:
+                    # if reddit api still gives a text post just post the link
+                    link = data[0]["data"]["children"][0]["data"]["url"]
                 
-                await ctx.send(link)
-                
+                    await ctx.send(link)           
 
+    except KeyError:
+            # subreddit unfound
+            embed = discord.Embed(color=color)
+            embed.add_field(name="Sub Error", value="Subreddit was not found.")
+            
+            await ctx.send(embed=embed)
+
+            
 bot.run(os.environ['TOKEN'])
