3353f7d930eaaf870b6cb17988feda3df8eadd51
Slick9000 <37941291+Slick9000@users.noreply.github.com>
2020-04-02 21:04:52 -0300

    Change prefix in discord activity

--- reddit.py
+++ reddit.py
@@ -24,7 +24,7 @@ async def on_ready():
 
     await bot.change_presence(
         activity=discord.Activity(
-            name="reddit :3 | r!help", type=discord.ActivityType.watching
+            name="reddit :3 | r~help", type=discord.ActivityType.watching
         )
     )
 
